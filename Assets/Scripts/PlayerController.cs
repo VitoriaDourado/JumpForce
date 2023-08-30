0b327621d5abd61039503648540e9348da9a079b
Vitoria Dourado <vickawaii14@gmail.com>
2023-08-30 00:26:57 -0300

    :sparkles: :feat: Adding scripts obstacles

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,6 +7,7 @@ public class PlayerController : MonoBehaviour
     private Rigidbody playerRb;
     public float jumpForce;
     public float gravityModifier;
+    public bool isOnGround = true;
     // Start is called before the first frame update
     void Start()
     {
@@ -17,14 +18,15 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        InputForce();
+        if(Input.GetKeyDown(KeyCode.Space) && isOnGround)
+        {
+            playerRb.AddForce(Vector3.up *jumpForce, ForceMode.Impulse);
+            isOnGround = false;
+        }
     }
 
-    void InputForce()
+    private void OnCollisionEnter(Collision collision)
     {
-        if(Input.GetKeyDown(KeyCode.Space))
-        {
-            playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
-        }
+        isOnGround = true;
     }
 }
